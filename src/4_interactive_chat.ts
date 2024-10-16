871bec5f90804e366c4b2f6adb98c728d68d609c
Jose Sandoya <joseivansandoya@gmail.com>
2024-10-15 22:46:56 -0500

    create an interactive chat

---
+++ src/4_interactive_chat.ts
@@ -0,0 +1,40 @@
+require('dotenv').config();
+
+import { input } from '@inquirer/prompts';
+import OpenAI from "openai";
+
+const apiKey = process.env.OPENAI_API_KEY;
+const openai = new OpenAI({
+  apiKey,
+});
+
+async function main() {
+  const initialAssistantMessage = 'What can I help with? (type "exit" to end assistant)';
+  let prompt = await input({ message: initialAssistantMessage });
+
+  const messages: OpenAI.ChatCompletionMessageParam[] = [
+    { role: "system", content: "You are a helpful assistant. Use the supplied tools to assist the user." },
+    { role: "assistant", content: initialAssistantMessage },
+    { role: "user", content: prompt },
+  ];
+
+  while (prompt !== 'exit') {
+    console.log('...');
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o",
+      messages: messages,
+    });
+
+    if (response) {
+      const content = response.choices[0].message.content;
+      console.log(`>>> Assistant: ${content}`);
+      prompt = await input({ message: 'Your response: (type "exit" to end assistant)' });
+      messages.push({ role: "assistant", content });
+      messages.push({ role: "user", content: prompt });
+    }
+  }
+
+  console.log('Goodbye!');
+}
+
+main();
